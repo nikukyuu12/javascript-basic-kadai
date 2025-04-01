17b68f6ecf153d553e6cf4a95b4ec0272687ac3e
nikukyuu12 <momp.h5678@gmail.com>
2025-04-02 01:23:40 +0900

    kadai_mdn_js修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,33 @@
-const today = new Date();
-const options = {
+const today =new Date() ;
+
+const year = today.getFullYear();
+const month = today.getMonth()+1;
+const date = today.getDate();
+
+console.log(year + "年" + month + "月" + date + "日");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const options = {
   year: "numeric",
   month: "long",
   day: "numeric",
 };
 
-console.log(today.toLocaleDateString("ja-JP-u-ca", options));
+console.log(today.toLocaleDateString("ja-JP-u-ca", options));   */
